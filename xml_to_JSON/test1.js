47f05ae0365be854d3a8d2ac877d68852069aadd
Mattias Lindström <mattias.lindstrom@gmail.com>
2017-07-27 15:06:58 -0500

    Add store object maker

--- xml_to_JSON/test1.js
+++ xml_to_JSON/test1.js
@@ -2,22 +2,29 @@
 
 window.onload = function () {
 
-    var http = new XMLHttpRequest();
+    var articles = new XMLHttpRequest();
+    var stores = new XMLHttpRequest();
+    var sortiment = new XMLHttpRequest();
     var myOptions = {
         attrsAsObject: false,
         textKey: 'key',
         // xmlns: false,
     };
 
-    http.onreadystatechange = function() {
-        if(http.readyState == 4 && http.status == 200) {
+    articles.open('GET', 'untitled.xml', true);
+    articles.send();
+    stores.open('GET', 'butiker.xml', true);
+    stores.send();
+
+    articles.onreadystatechange = function() {
+        if(articles.readyState == 4 && articles.status == 200) {
             // Converts the xml to JSON.
-            result = xmlToJSON.parseString(http.response, myOptions);
+            result = xmlToJSON.parseString(articles.response, myOptions);
             // Returns a list of objects representing the articles
             var list = result.artiklar[0].artikel;
             // Returns a list of only the beer in the selection. The objects
             // are cleaned up to be easy to use.
-            var beer = list.filter(a => a.Varugrupp[0].key == "Öl")
+            var beer = list.filter(a => a.Varugrupp[0].key == 'Öl')
                 .reduce(function(acc, current) {
                     var article = {};
                     for (var key in current) {
@@ -28,13 +35,36 @@ window.onload = function () {
                     }
                     return acc.concat(article);
                 }, []);
-
         }
     };
-    http.open("GET", "untitled.xml", true);
-    http.send();
+
+    stores.onreadystatechange = function() {
+        if(stores.readyState == 4 && stores.status == 200) {
+            // Converts the xml to JSON.
+            result = xmlToJSON.parseString(stores.response, myOptions);
+            // Returns a list of objects representing the stores
+            var list = result.ButikerOmbud[0].ButikOmbud;
+            // Cleaning up the objects to make them easy to use.
+            var butiker = list.reduce(function(acc, current) {
+                    var butik = {};
+                    for (var key in current) {
+                        if (key == '_attrtype') {
+                            continue;
+                        }
+                        // Replaces every [null] value with '' and
+                        // makes the other values a string.
+                        butik[key] = typeof current[key][0].key ==
+                        'object' ? '' : current[key][0].key;
+                    }
+                    return acc.concat(butik);
+                }, []);
+            console.log(JSON.stringify(butiker));
+        }
+    };
 
 };
+
+
 
 // 0 - request not initialiezed
 // 1 - request has been set up
