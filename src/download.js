44214a80b675a4c10e576ee8f8251ac9265a5407
Mattias Lindström <mattias.lindstrom@gmail.com>
2017-08-08 15:16:56 -0500

    Refractor code and make it cleanup and handle all files

--- src/download.js
+++ src/download.js
@@ -2,12 +2,28 @@
 
 var https = require('https');
 var fs = require('fs');
+var cleanup = require('./JSONCleanup.js');
+var fetch = require('node-fetch')
 
-const PRODUCTS = 'https://www.systembolaget.se/api/assortment/products/xml';
+var urls = {
+    'products': 'https://www.systembolaget.se/api/assortment/products/xml',
+    'stores': 'https://www.systembolaget.se/api/assortment/stores/xml',
+    'selection': 'https://www.systembolaget.se/api/assortment/stock/xml',
+};
 
-https.get(PRODUCTS, function(response) {
-    var data = '';
-    response.setEncoding('utf8');
-    response.on('data', (chunk) => {data += chunk;} );
-    response.on('end', () => fs.writeFile('products.xml', data));
+function download(filename, url) {
+    https.get(url, function(response) {
+        var data = '';
+        response.setEncoding('utf8');
+        response.on('data', (chunk) => data += chunk);
+        response.on('end', () => {
+            fs.writeFileSync(filename + '.xml', data);
+            cleanup[filename]('./' + filename + '.xml', './' + filename + '.json');
+                    });
 }).on('error', (e) => console.log(e));
+}
+
+
+download('stores', urls.stores);
+
+
