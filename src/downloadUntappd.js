8ee6b52e63d2acf0c661ca6b95228069f083ac84
Mattias Lindström <mattias.lindstrom@gmail.com>
2017-08-31 14:38:06 -0500

    Intent code

--- src/downloadUntappd.js
+++ src/downloadUntappd.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 var request = require('request');
 var https = require('https');
-var beerList = require('./products.json');
+var beerList = require('./products2.json');
 var fs = require('fs');
 
 var client_id = '&client_id=' + process.env.CLIENT_ID;
@@ -19,25 +19,30 @@ var bid = 'Schlappeseppel Dunkel';
 //         res.on('data', (d) => str += d);
 //         res.on('end', () => console.log(JSON.parse(str).response.beers.items[0].beer.bid));});
 // Object.keys(beerList).forEach(function(a) {
-    for (var a = 0; a < 2; a++) {
+for (var a = 0; a < 4; a++) {
+
     if(beerList[a].bid) return;
     var namn = beerList[a].Namn;
     namn += beerList[a].Namn2 ? ' ' + beerList[a].Namn2 : '';
-    https.get(address + beer_method + encodeURIComponent(namn) +
-              client_id + client_secret, function(res) {
-        res.setEncoding('utf8');
-        var str = '';
-        res.on('data', (d) => str += d);
-        res.on('end', () => {
-                            console.log(str);
-                            beerList[a].bid =
-                            JSON.parse(str).response.beers.items[0] ?
-                            JSON.parse(str).response.beers.items[0].beer.bid :
-                            ''});
+
+    https.get(address + beer_method +
+              encodeURIComponent(namn) +
+              client_id + client_secret,
+
+        function(res) {
+            res.setEncoding('utf8');
+            var str = '';
+            res.on('data', (d) => str += d);
+            res.on('end', () => {
+                beerList[a].bid =
+                JSON.parse(str).response.beers.items[0] ?
+                JSON.parse(str).response.beers.items[0].beer.bid :
+                '';
+                console.log(beerList[a].bid);
+            });
 });
-    
-    if (a >= 6) {
-        console.log(beerList);
+
+    if (a >= 3) {
         fs.writeFile('./products2.json', JSON.stringify(beerList, null, 4));
 }
 }
