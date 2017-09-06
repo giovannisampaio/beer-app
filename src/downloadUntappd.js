29280be1f5244814123ae3990101508725d8897d
Mattias Lindström <mattias.lindstrom@gmail.com>
2017-09-06 13:22:50 -0500

    Remove old code, refractor iterate():
    Refractor iterate() into three different functions: iterate(), timer(),
    and resolver().

--- src/downloadUntappd.js
+++ src/downloadUntappd.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 var request = require('request');
 var https = require('https');
-var beerList = require('./products3.json');
+var beerList = require('./products5.json');
 var fs = require('fs');
 
 var client_id = '&client_id=' + process.env.CLIENT_ID;
@@ -107,102 +107,31 @@ function updateInfo(info, num, arr) {
     });
 }
 
-var iterate = (cur, ind, arr1) => {
+function timer(func, cur, ind, arr1) {
     return new Promise(resolve =>
         setTimeout(() => {
-            var name = getName(ind, arr1);
-            resolve(getBID(name)
-            .then((BID) => updateBID(BID, ind, arr1))
-            .then((BID) => getInfo(BID, name)
-            .then((info1) =>  updateInfo(info1, ind, arr1))));
-    }, (counter % 100 == 1) && (counter > 1) ? 60*60*1000 : 0));};
+            resolve(func(cur, ind, arr1));
+        }, (counter % 99 === 0) ? 60*60*1000 : 0)
+        );
+}
+
+function resolver(cur, ind, arr1) {
+    var name = getName(ind, arr1);
+    return getBID(name)
+    .then((BID) => updateBID(BID, ind, arr1))
+    .then((BID) => getInfo(BID, name)
+    .then((info1) =>  updateInfo(info1, ind, arr1)));
+}
+
+var iterate = (cur, ind, arr1) => {
+    return timer(resolver, cur, ind, arr1);
+};
 
 
 var ready = beerList.map(iterate);
 var results = Promise.all(ready);
 results.then(() => {
-    fs.writeFile('./products2.json', JSON.stringify(beerList, null, 4));
+    fs.writeFile('./products4.json', JSON.stringify(beerList, null, 4));
     fs.writeFile('./noBID.json', JSON.stringify(noBID, null, 4));
     fs.writeFile('./noInfo.json', JSON.stringify(noInfo, null, 4));
 });
-// beerList.map((cur, ind, arr1) => {
-//     setTimeout(() => {
-//         getBID(getName(ind, arr1))
-//         .then((x) => updateBID(x, ind, arr1));
-//     }, ind % 100 == 1? 60*60*1000 : 0);
-// }).then(() => {fs.writeFile('./products2.json', JSON.stringify(beerList, null, 4));});
-// getBID(getName(0))
-// .then((x) => updateBID(x, 0))
-// .then(() => {fs.writeFile('./products2.json', JSON.stringify(beerList, null, 4));});
-
-
-// function iterate(num) {
-//     return new Promise(function(resolve, reject) {
-//         for (var i = 0; i < num; i++) {
-//             getBID(getName(i)).then((x) => updateBID(x, i).then(() => {i === (num - 1) ? resolve() : false}));
-//             console.log(i);
-//         }
-//     });
-// }
-// iterate(4).then(() => {fs.writeFile('./products2.json', JSON.stringify(beerList, null, 4));});
-// getBID(getName(0)).then((x) => updateBID(x, 0)).then(fs.writeFile('./products2.json', JSON.stringify(beerList, null, 4)));
-// getBID(getName(0)).then(console.log);
-// function jiterate() {
-
-// }
-
-// https.get(address + beer_method + bid + client_id + client_secret, function(res) {
-//         res.setEncoding('utf8');
-//         var str = '';
-//         res.on('data', (d) => str += d);
-//         res.on('end', () => console.log(JSON.parse(str).response.beers.items[0].beer.bid));});
-// Object.keys(beerList).forEach(function(a) {
-// for (var a = 0; a < 4; a++) {
-
-//     if(beerList[a].bid) return;
-//     var namn = beerList[a].Namn;
-//     namn += beerList[a].Namn2 ? ' ' + beerList[a].Namn2 : '';
-
-//     https.get(address + beer_method +
-//               encodeURIComponent(namn) +
-//               client_id + client_secret,
-
-//         function(res) {
-//             res.setEncoding('utf8');
-//             var str = '';
-//             res.on('data', (d) => str += d);
-//             res.on('end', () => {
-//                 beerList[a].bid =
-//                 JSON.parse(str).response.beers.items[0] ?
-//                 JSON.parse(str).response.beers.items[0].beer.bid :
-//                 '';
-//             });
-// });
-
-//     if (a >= 3) {
-        // fs.writeFile('./products2.json', JSON.stringify(beerList, null, 4));
-// }
-// }
-// function update(num) {
-//     for (var a = 0; a < num; a++) {
-//         if(beerList[a].bid) return;
-//         var namn = beerList[a].Namn;
-//         namn += beerList[a].Namn2 ? ' ' + beerList[a].Namn2 : '';
-
-//         https.get(address + beer_method +
-//               encodeURIComponent(namn) +
-//               client_id + client_secret,
-
-//         function(res) {
-//             res.setEncoding('utf8');
-//             var str = '';
-//             res.on('data', (d) => str += d);
-//             res.on('end', () => {
-//                 beerList[a].bid =
-//                 JSON.parse(str).response.beers.items[0] ?
-//                 JSON.parse(str).response.beers.items[0].beer.bid :
-//                 '';
-//             });
-//     });
-// }
-// }
